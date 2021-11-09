43691fc869304d4662f5b03503e9895f5d525de9
sysun0813 <37135552+sysun0813@users.noreply.github.com>
2021-11-09 14:43:49 +0900

    Player statistics setting
    Player의 stat 설정 및 상속

--- Assets/Scripts/Main_Character_Move.cs
+++ Assets/Scripts/Main_Character_Move.cs
@@ -2,10 +2,51 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Main_Character_Move : MonoBehaviour
+public interface CharacterInterface
 {
-    public float speed=1;
-    public float radius;
+    string Name { get; }
+
+    float MaxHP { get; }
+
+    float Defense { get; }
+
+    float MoveSpeed { get; }
+
+    float AttackDelay { get; }
+
+    float AttackPower { get; }
+
+    float AttackRange { get; }
+
+}
+
+public class Main_Character_Move : MonoBehaviour , CharacterInterface
+{
+    [Header("플레이어 스탯")]
+
+    public string _name;
+    public string Name { get { return _name; } }
+
+
+    public float maxHP;
+    public float MaxHP { get { return maxHP; } }
+
+
+    public float attackpower;
+    public float AttackPower { get { return attackpower; } }
+
+
+    public float defense;
+    public float Defense { get { return defense; } }
+
+    public float attackdelay;
+    public float AttackDelay { get { return attackdelay; } }
+
+    public float movespeed;
+    public float MoveSpeed { get { return movespeed; } }
+
+    public float range;
+    public float AttackRange { get { return range; } }
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +56,7 @@ public class Main_Character_Move : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(!(Physics2D.OverlapCircle(transform.position, radius, LayerMask.GetMask("EnemyCharacter"))))
+        if(!(Physics2D.OverlapCircle(transform.position, range, LayerMask.GetMask("EnemyCharacter"))))
         {
             Move();
         }
@@ -24,6 +65,6 @@ public class Main_Character_Move : MonoBehaviour
 
     public void Move()
     {
-        transform.position += Vector3.right * speed * Time.deltaTime;
+        transform.position += Vector3.right * movespeed * Time.deltaTime;
     }
 }
