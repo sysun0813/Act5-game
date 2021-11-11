edaadbe70e2d804ea9b07abebf7649e0b0bd83d9
AnsirH <ansir0211@gmail.com>
2021-11-12 00:28:56 +0900

    카메라 스크립트 및 심판 애니메이션 수정

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform targetTransform;
+
+    float cameraHalfHeight, cameraHalfWidth;
+
+    public float smoothSpeed = 3f;
+
+    public float limitMinX, limitMaxX, limitMinY, limitMaxY;
+
+    private void Start()
+    {
+        cameraHalfHeight = Camera.main.orthographicSize;
+        cameraHalfWidth = Camera.main.aspect * Camera.main.orthographicSize;
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 desiredPosition = new Vector3(
+            Mathf.Clamp(targetTransform.position.x, limitMinX + cameraHalfWidth, limitMaxX - cameraHalfWidth),
+            transform.position.y,
+            -10f);
+
+        transform.position = Vector3.Lerp(transform.position, desiredPosition, Time.deltaTime * smoothSpeed);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(new Vector2(limitMinX, limitMinY), new Vector3(limitMaxX, limitMinY));
+        Gizmos.DrawLine(new Vector2(limitMinX, limitMaxY), new Vector3(limitMaxX, limitMaxY));
+        Gizmos.DrawLine(new Vector2(limitMinX, limitMinY), new Vector3(limitMinX, limitMaxY));
+        Gizmos.DrawLine(new Vector2(limitMaxX, limitMinY), new Vector3(limitMaxX, limitMaxY));
+    }
+}
