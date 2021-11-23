150dffa05d9ff08bd8c71992e12d482ee1ce3070
Sooyoung Sun <sysun0813@naver.com>
2021-11-24 02:28:03 +0900

    refree movement
    상황(조건)에 맞는 refree movement setting

--- Assets/Scripts/Character/Referee.cs
+++ Assets/Scripts/Character/Referee.cs
@@ -4,5 +4,41 @@ using UnityEngine;
 
 public class Referee : MonoBehaviour
 {
+    public Transform cam;
 
+    private Transform refe;
+
+    private bool tf = false;
+
+    StageManager stageManager;
+
+    private void Start()
+    {
+        refe = GetComponent<Transform>();
+        stageManager = GameObject.Find("StageManager").GetComponent<StageManager>();
+        refe.position = new Vector2(cam.position.x + 4f, -2.66f);
+    }
+
+    private void Update()
+    {
+        if (stageManager.currentPlayers.Count > 0)
+        {
+            tf = true;
+            refe.position = new Vector2(stageManager.currentPlayers[0].transform.position.x + 4.0f, -2.66f);
+        }
+        else if (tf = true && (stageManager.players.Count == 0 || true /*true 대신 아군 깃발 베었는지 참거짓 코드 추가*/ ))
+        {
+            //깃발 오른쪽으로 들기
+        }
+        else if (tf = true && (stageManager.enemies.Count == 0 || true /*true 대신 적군 깃발 베었는지 참거짓 코드 추가*/))
+        {
+            //깃발 왼쪽으로 들기
+        }
+
+        else
+        {
+            refe.position = new Vector2(cam.position.x + 4f, -2.66f);
+        }
+
+    }
 }
