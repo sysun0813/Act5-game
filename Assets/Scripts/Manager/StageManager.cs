38aa807efac3bb743584b7fd1a93e5610cec6ba0
AnsirH <ansir0211@gmail.com>
2021-11-28 02:13:13 +0900

    StageManager 수정중

--- Assets/Scripts/Manager/StageManager.cs
+++ Assets/Scripts/Manager/StageManager.cs
@@ -39,22 +39,27 @@ public class StageManager : MonoBehaviour
     [SerializeField] List<Enemy> easyEnemies;
     [SerializeField] List<Enemy> normalEnemies;
     //[SerializeField] List<Enemy> hardEnemies;
+    [SerializeField] List<Enemy> bossEnemies;
 
     int currentStage = 1;
 
-    StageInformation previousStageInfo;
-    StageInformation currentStageInfo;
-    StageInformation nextStageInfo;
-    StageInformation returnStageInfo;
+    [SerializeField] StageInformation previousStageInfo;
+    [SerializeField] StageInformation currentStageInfo;
+    [SerializeField] StageInformation nextStageInfo;
+    [SerializeField] StageInformation returnStageInfo;
 
     private void Start()
     {
-        endStage.OnEndStage += ReStartStage;
+        endStage.OnEndStage += LoadStage;
+
+        currentStageInfo = MakeStage(currentStage);
+        nextStageInfo = MakeStage(currentStage + 1);
+
         StartCoroutine(StartSpawn());
     }
 
     // EndStage ÇÔ¼ö
-    void ReStartStage()
+    void LoadStage()
     {
         while(currentPlayers.Count > 0)
         {
@@ -95,20 +100,53 @@ public class StageManager : MonoBehaviour
         yield return StartCoroutine(StartSpawn());
     }
 
-    StageInformation MakeStage()
+    StageInformation MakeStage(int stageNum)
     {
         StageInformation stage = new StageInformation
         {
+            map = maps[Random.Range(0, maps.Count)],
+            enemies = new List<Enemy>()
         };
+
+        if(stageNum < 10)
+        {
+            stage.enemies.Add(GetRandomEnemy(easyEnemies));
+        }
+        else if (stageNum < 50)
+        {
+            for(int i = 0; i < 2; i++)
+            {
+                stage.enemies.Add(GetRandomEnemy(easyEnemies));
+            }
+        }
+        else if(stageNum == 50)
+        {
+            stage.enemies.Add(GetRandomEnemy(bossEnemies));
+            for(int i = 0; i < 2; i++)
+            {
+                stage.enemies.Add(GetRandomEnemy(easyEnemies));
+            }
+        }
+        else if(stageNum < 100)
+        {
+            int randCount = Random.Range(3, 5);
+            for(int i = 0; i < randCount; i++)
+            {
+                stage.enemies.Add(GetRandomEnemy(easyEnemies));
+            }
+        }
+
         return stage;
     }
 
-    void GetEnemies()
+    Enemy GetRandomEnemy(List<Enemy> enemiesList)
     {
-
+        return enemiesList[Random.Range(0, enemiesList.Count)];
     }
+
 }
 
+[System.Serializable]
 public class StageInformation
 {
     public GameObject map;
